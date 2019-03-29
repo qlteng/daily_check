b275422002319c6596bee430d433714183278f9c
qlteng <tql_940205@163.com>
2019-03-29 19:41:46 +0800

    python2.7

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 dir=`pwd`
 today=`date -d today +"%Y-%m-%d"`
-/usr/bin/python ${dir}/src/daily_recorder.py --dir ${dir}>>${dir}/log/daily_check.log 2>&1 &
+/usr/bin/python2.7 ${dir}/src/daily_recorder.py --dir ${dir}>>${dir}/log/daily_check.log 2>&1 &

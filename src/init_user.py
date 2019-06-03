d82a9e235c028aeb4744f5e6e1abfb4662155509
qlteng <tql_940205@163.com>
2019-06-03 13:49:22 +0800

    50

--- src/init_user.py
+++ src/init_user.py
@@ -4,8 +4,8 @@ class init_user:
 
     def __init__(self):
 
-        self.userlist = ['田泽英','牛红峰','张兆哲','李永明','Asif','ashil','雷宗木','丁洁','Nomaan Khan','Muhammad','赵宇轩','魏昂','朱蓉蓉','闫东超','Shafiq Rai','张陈然','牛军锋','赵颖慧']
-        self.sp = {}
+        self.userlist = ['滕千礼','蔡志波','王春燕','周存理','张兆哲','李永明','雷宗木','丁洁','Nomaan Khan','赵宇轩','魏昂','朱蓉蓉','闫东超','Shafiq Rai','张陈然','牛军锋','赵颖慧']
+        self.sp = {'牛红峰':50,'ashil':50,'Muhammad':50,'Asif':50}
 
 if __name__ == '__main__':
 
